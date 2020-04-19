5e34fe5002a3314fe038edc6656f6c8e84e93b4e
Bren-lap <brennenbates2859@gmail.com>
2020-04-19 03:57:11 -0500

    Implemented first version of role based access control user authentication
    Keeping site to a single page format.  Using role based access control to manipulate the page based on user permissions.  Currently, if no user exists it will ask the user to login.  If user exists with visitor home page view access it renders profile info.  If user exists with visitor home page edit access, render buttons to submit new recipe or new blog entries.

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,16 +1,66 @@
-import React, { useState } from "react";
+import React, { Fragment } from "react";
 import '../Home/home.css'
+import {useAuth0} from '../../react-auth0-spa';
+import Can from "../../components/Can";
 
-function home(props) {
+function Home() {
+    const { loading, user } = useAuth0();
 
-  
+  if (loading) {
+    return <div>Loading...</div>;
+  } else if (!user) {
+    return (
+        <div>
+            <h1>Welcome to the Family Recipe Collection</h1>
+            <h2>Please Log In</h2>
+        </div>
+    )
+  } else {
 
-    return <div>
+ 
 
-    <h1>Hello Welcome to The Family Recipe Collection</h1>
-          
-    </div>;
-}
+  return (
 
+    <div>
 
-export default home;
+    <Can
+    role= {user["https://grannie-b/role"]}
+    perform="home-page:edit"
+    yes={() => (
+        <Fragment>
+            <button>New Blog</button>
+            <button>New Recipe</button>
+        </Fragment>
+    )}
+    no={() => 
+    
+        
+        <Can
+    role= {user["https://grannie-b/role"]}
+    perform="home-page:visit"
+    yes={() => (
+
+    <Fragment>
+      <img src={user.picture} alt="Profile" />
+
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+      <p>{user.role}</p>
+      <code>{JSON.stringify(user, null, 2)}</code>
+    </Fragment>
+
+  )}
+    no={() => <h2>Hello{' '+user.name}, welcome to the family recipe collection!</h2>}
+    />
+    }
+    />
+
+    
+
+    </div>
+   
+  );
+};
+};
+
+export default Home;
