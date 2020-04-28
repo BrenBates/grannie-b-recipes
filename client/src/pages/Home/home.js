ff676b2b93aa657b86f2e68c109b69341eafca06
Bren <brennenbates2859@gmail.com>
2020-04-28 00:47:16 -0500

    Revert "troubleshooting auth0 and heroku deploy"
    This reverts commit 8e7c5c1ef8a6dcb1643122893eb2568315474aba.

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from "react";
 import '../Home/home.css'
+import {useAuth0} from '../../react-auth0-spa';
 import Navbar from "../../components/Navbar/navbar";
 
 
 function Home() {
 
+  const {loading} = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
 
+    <Fragment>
 
     <div>
     <Navbar />
@@ -16,7 +25,7 @@ function Home() {
         <h1>{process.env.REACT_APP_AUTH_CLIENT}</h1>
     </div>
 
-
+    </Fragment>
    
   );
 
