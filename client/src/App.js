65e38e518e37e70e75ef1bd13a4a0b86e1c0dbda
Bren <brennenbates2859@gmail.com>
2020-04-29 02:59:54 -0500

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/home";
-import {useAuth0} from './react-auth0-spa';
 import Admin from "./pages/Admin/admin";
 import NewBlog from "./pages/NewBlog/newblog";
 import NewRecipe from "./pages/NewRecipe/newrecipe";
