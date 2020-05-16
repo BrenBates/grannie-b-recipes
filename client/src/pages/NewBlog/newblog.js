f974219e4a4f4a448749bc0d00a81391d461f503
Bren-lap <brennenbates2859@gmail.com>
2020-05-16 11:52:05 -0500

    Beginnings of blog gui programming

--- client/src/pages/NewBlog/newblog.js
+++ client/src/pages/NewBlog/newblog.js
@@ -16,31 +16,103 @@ import {
 function NewBlog(props) {
 
 const [selector, setSelector] = useState('');
+const [listCount, setListCount] = useState(0);
 const [blogItem, newBlogItem] = useState([]);
 
 const handleClick = value => {
-  console.log(value)
+  setSelector(value)
 }
+
+
+useEffect(() => {
+  renderSelection()
+},[selector]);
+
+useEffect(() => {
+  renderList()
+},[listCount]);
+
+
+const renderSelection = (list) =>  {
+
+  if(!list) {
+    return (
+      <>
+       
+        <textarea>
+        </textarea>
+        <button>Add</button>
+      </>
+         )
+        }
+
+  if(list) { 
+    return (
+      <>
+      <button onClick={() => setListCount(listCount+1)}>+</button>
+      
+       {renderList()}
+        
+      </>
+    )
+  }
+ 
+}
+
+
+const renderList = () => {
+  for(let i = 0; i < listCount; i++) {
+    return (
+      <li><textarea></textarea></li>
+    )
+  }
+}
+
 
 return (
 
-  <>
+   <Container>
 
-  <button onClick={() => handleClick('h1')}>h1</button>
-  <button value="h2">h2</button>
-  <button value="h3">h3</button>
-  <button value="h4">h4</button>
-  <button value="h5">h5</button>
-  <button value="h6">h6</button>
-  <button value="ul">Bullet List</button>
-  <button value="ol">Numbered List</button>
-  <button value="li">List Item</button>
-  <button value="p">Paragraph</button>
+     <Row>
+
+      <button onClick={() => handleClick('p')}>Paragraph</button>
+      <button onClick={() => handleClick('h1')}>h1</button>
+      <button onClick={() => handleClick('h2')}>h2</button>
+      <button onClick={() => handleClick('h3')}>h3</button>
+      <button onClick={() => handleClick('h4')}>h4</button>
+      <button onClick={() => handleClick('h5')}>h5</button>
+      <button onClick={() => handleClick('h6')}>h6</button>
+      <button onClick={() => handleClick('ul')}>Bullet List</button>
+      <button onClick={() => handleClick('ol')}>Number List</button>
+      
+      </Row>
+
+      <Row>
+        <Col xs="6">
+
+
+          {selector === 'h1' ? <h1>{renderSelection()}</h1> 
+          : selector === 'h2' ? <h2>{renderSelection()}</h2>
+          : selector === 'h3' ? <h3>{renderSelection()}</h3>
+          : selector === 'h4' ? <h4>{renderSelection()}</h4>
+          : selector === 'h5' ? <h5>{renderSelection()}</h5>
+          : selector === 'h6' ? <h6>{renderSelection()}</h6>
+          : selector === 'p' ? <p>{renderSelection()}</p>
+          : selector === 'ul' ? <ul>{renderSelection('list')}</ul>
+          : selector === 'ol' ? <ol>{renderSelection('list')}</ol>
+          
+          : <p>Select an item to add</p>}
+           
+          
+
+        </Col>
+      </Row>    
+
+    </Container>
 
 
 
-
-  </>
+ 
 
 )
     
